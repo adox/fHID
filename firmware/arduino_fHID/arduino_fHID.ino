c4199d8263af09c734dde607b68f28aae5454fc3
adox <adam2c@poczta.onet.pl>
2012-06-07 15:29:43 +0200

    Working intervals measure

--- firmware/arduino_fHID/arduino_fHID.ino
+++ firmware/arduino_fHID/arduino_fHID.ino
@@ -1,67 +1,79 @@
 #include "RfidHID.h"
 
 #define THR 21
+//#define _FHID_DEBUG_
 
-const byte envInPin = 7;
-const byte nOE = 2;  // n output enable
-unsigned long duration[5];
+#define DUR_LEN 20
+
+const byte envInPin = 2;
+const byte nOE = 7;  // n output enable
+unsigned long duration[DUR_LEN];
+unsigned long time;
 int i;
+byte dataReady;
 
 char state;
 enum {H1, H2, H3, READ5, PRINTOUT};
 
 RfidHID hid(nOE);
 
-/* Measures the length (in microseconds) of a pulse on the pin; state is HIGH
- * or LOW, the type of pulse to measure.  Works on pulses from 2-3 microseconds
- * to 3 minutes in length, but must be called at least a few dozen microseconds
- * before the start of the pulse. */
-unsigned int myPulseIn(uint8_t pin, uint8_t state)
-{
-        // cache the port and bit of the pin in order to speed up the
-        // pulse width measuring loop and achieve finer resolution.  calling
-        // digitalRead() instead yields much coarser resolution.
-        uint8_t bit = digitalPinToBitMask(pin);
-        uint8_t port = digitalPinToPort(pin);
-        uint8_t stateMask = (state ? bit : 0);
-        unsigned int width = 0; // keep initialization out of time critical area
-        
-        // convert the timeout from microseconds to a number of times through
-        // the initial loop; it takes 16 clock cycles per iteration.
-        unsigned int numloops = 0;
-        unsigned int maxloops = 2048;
-        
-        // disable interrupts
-        cli();
-        
-        // wait for any previous pulse to end
-        while ((*portInputRegister(port) & bit) == stateMask)
-                if (numloops++ == maxloops)
-                        return 0;
-        
-        // wait for the pulse to start
-        while ((*portInputRegister(port) & bit) != stateMask)
-                if (numloops++ == maxloops)
-                        return 0;
-        
-        // wait for the pulse to stop
-        while ((*portInputRegister(port) & bit) == stateMask)
-                width++;
 
-        // enable interrupts
-        sei();
-        
-        return width; 
+void checkTiming() {
+
+  if(i < DUR_LEN) {
+    duration[i++] = micros() - time;
+    time = micros();
+    //duration[i++] = i;
+  } else {
+    dataReady = 1;
+  }
+
+}
+
+void readData() {
+  unsigned long startTime;
+  
+  #ifdef _FHID_DEBUG_
+  Serial.println("Entering readData()");
+  #endif
+  
+  dataReady = 0;
+  i = 0;
+  hid.genOE(1);  
+  attachInterrupt(0, checkTiming, RISING); 
+
+  #ifdef _FHID_DEBUG_
+  Serial.println("Waiting for data to be ready...");  
+  #endif
+  
+  startTime = millis();
+  while(dataReady == 0) {
+    if(millis() - startTime > 3000) {
+      Serial.println("Timeout!");
+      break;
+    }
+  };
+  
+  #ifdef _FHID_DEBUG_
+  Serial.println("Data is ready");
+  #endif
+
+  detachInterrupt(0);
+  hid.genOE(0);      
+  
+  for(i=1; i<DUR_LEN; i++) {
+    Serial.print(duration[i], DEC);  
+    Serial.print(" ");
+  }
+  Serial.println();
+  
+  #ifdef _FHID_DEBUG_
+  Serial.println("Leaving readData()");  
+  #endif
 }
 
 void setup() {
   pinMode(envInPin, INPUT);
-
-  
-
-  pinMode(nOE, OUTPUT);
-  state = H1;
-  digitalWrite(nOE, 1);
   
   Serial.begin(9600);
   Serial.println("fHID ready");
@@ -69,10 +81,6 @@ void setup() {
 }
 
 void loop() {
-  
-  hid.genOE(1);
-  delay(100);
-  hid.genOE(0);
-  delay(100);
-  
+  readData();
+  delay(1000);
 }
