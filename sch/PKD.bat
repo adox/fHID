e0daadbbc6e81f8fd05f7bf38ee923940c50f7a6
adox <adam2c@poczta.onet.pl>
2012-08-03 15:43:02 +0200

    Removed local conflicted files

--- sch/PKD.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-"pvengine.exe" +W320 +H240 +FN +L"c:/windows/fonts" +L"C:/Program Files/EAGLE-6.2.0/ulp//../povray" +O"C:/Projekty/fHID/sch/PKD.png" /RENDER "C:/Projekty/fHID/sch/PKD.pov" 
